cc31f562f5d6eaeaa7b0a1e6d0858ff9d7999c64
Barbara Robba <barbara.robba@gmail.com>
2016-10-26 13:36:11 +0200

    Nadaljevanje

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -3,16 +3,20 @@ import requests
 def zapisi_vsebino():
     #osnovni naslov : http://home.izum.si/cobiss/top_gradivo/
     vsebina = []
-    for i in range(2,15): #šteje po letih
-        for j in range(1,12): #šteje po mesecih
-    a = requests.get('http://home.izum.si/cobiss/top_gradivo/default.asp?l=20'+'{num:02d}'.format(num=i)+'&m='+'{num:02d}'.format(num=j)+'&tipg=m%25&Submit=IZDELAJ+SEZNAM', auth=('user', 'pass'))
-    vsebina.append(a.text)
+    for i in range(2,16): #šteje po letih
+        if i<=15:
+            for j in range(1,12): #šteje po mesecih
+            a = requests.get('http://home.izum.si/cobiss/top_gradivo/default.asp?l=20'+str(j).zfill(2)+'&m='+str(i).zfill(2)+'&tipg=m%25&Submit=IZDELAJ+SEZNAM', auth=('user', 'pass'))
+            vsebina.append(a.text)
+        else: #za leto 2016 imamo podatke samo do vlkjučno septembra
+            for j in range(1, 9):  # šteje po mesecih
+                a = requests.get(
+                    'http://home.izum.si/cobiss/top_gradivo/default.asp?l=20' + 'str(j).zfill(2)' + '&m=' + 'str(i).zfill(2)' + '&tipg=m%25&Submit=IZDELAJ+SEZNAM', auth=('user', 'pass'))
+                vsebina.append(a.text)
     return (vsebina)
 
 
-#a = requests.get('http://home.izum.si/cobiss/top_gradivo/default.asp?l=2003&m=04&tipg=m%25&Submit=IZDELAJ+SEZNAM')
-# print (a.text)
-#'http://home.izum.si/cobiss/top_gradivo/default.asp?l=20'+'{num:02d}'.format(num=i)+'&m='+'{num:02d}'.format(num=i)+'&tipg=m%25&Submit=IZDELAJ+SEZNAM'
-
-
 print(zapisi_vsebino())
+
+
+
