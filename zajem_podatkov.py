ca42c177a45fcd230c725fa60e6b10381b780f76
Barbara Robba <barbara.robba@gmail.com>
2016-12-28 21:39:22 +0100

    popravila izdelavo tabele in pričela z analizo podatkov

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -1,6 +1,8 @@
 import re
 import csv
 import requests
+from os import listdir
+from os.path import isfile, join
 
 
 def zapisi_vsebino():
@@ -41,15 +43,18 @@ def pocisti_delo(delo):
     podatki['avtor'] = podatki['avtor']
     podatki['izposoja'] = int(podatki['izposoja'])
     podatki['rezervacija'] = int(podatki['rezervacija'])
+    #for a in podatki:
+    """a['avtor'] = a['avtor'].capitalize()
+    a['naslov']= a['naslov'].capitalize()"""
+    z = podatki['avtor'].split(' ')
+    z[0] = z[0].capitalize()
+    a = ''
+    for i in z:
+        a += i + ' '
+    podatki['avtor'] = a
     return podatki
 
 
-def zapisi_csv(slovarji, imena_polj, ime_datoteke):
-    with open(ime_datoteke, 'w') as csv_dat:
-        writer = csv.DictWriter(csv_dat, fieldnames= imena_polj)
-        writer.writeheader()
-        for slovar in slovarji:
-            writer.writerow(slovar)
 
 def beri(ime_datoteke):
     a = open(ime_datoteke, 'r', encoding='utf-8')
@@ -58,25 +63,31 @@ def beri(ime_datoteke):
     return b
 
 
-def zapisi_csv(ime_txt_datoteke, ime_csv_datoteke):
-    #vzame vsebino prve datoteke, poišče podatke in oblikuje tabelo, ki jo shrani v csv datoteko
-    vsebina = beri(ime_txt_datoteke)
-    podatki = re.finditer(regex_dela, vsebina)
-    slovarji = []
-    for a in podatki:
-        slovarji.append(pocisti_delo(a))
-    with open(ime_csv_datoteke, 'w', encoding = 'utf-8') as csv_dat:
-        writer = csv.DictWriter(csv_dat, fieldnames=['naslov', 'avtor', 'izposoja', 'rezervacija'])
+def enotni_csv():
+    txt_files = [f for f in listdir('.\\') if isfile(join('.\\', f)) and f[-4:]=='.txt']
+    with open('podatki.csv', 'w', encoding='utf-8') as csv_dat:
+        writer = csv.DictWriter(csv_dat, fieldnames=['naslov', 'avtor', 'izposoja', 'rezervacija', 'leto', 'mesec'])
         writer.writeheader()
-        for slovar in slovarji:
-            writer.writerow(slovar)
+        for f in txt_files:
+            vsebina = beri(f)
+            podatki = re.finditer(regex_dela, vsebina)
+            slovarji = []
+            leto = f[4:8]
+            mesec = f[13:-4]
+            for a in podatki:
+                b = pocisti_delo(a)
+                b['leto'] = leto
+                b['mesec']= mesec
+                slovarji.append(b)
+            for slovar in slovarji:
+                writer.writerow(slovar)
 
 #naredimo .txt file z vsebino strani
-vsebina = zapisi_vsebino()
-shrani_vsebino(vsebina)
+#vsebina = zapisi_vsebino()
+#shrani_vsebino(vsebina)
 
 #naredimo tabele
-for l in range(2, 17):  # šteje po letih
+"""for l in range(2, 17):  # šteje po letih
     if l <= 15:
         for m in range(1, 13):  # šteje po mesecih
             zapisi_csv('leto20{}mesec{}.txt'.format(str(l).zfill(2),m), 'leto20{}mesec{}.csv'.format(str(l).zfill(2),m))
@@ -84,3 +95,5 @@ for l in range(2, 17):  # šteje po letih
     else:  # za leto 2016 imamo podatke samo do vključno septembra
         for m in range(1, 10):  # šteje po mesecih
             zapisi_csv('leto20{}mesec{}.txt'.format(str(l).zfill(2), m), 'leto20{}mesec{}.csv'.format(str(l).zfill(2),m))
+"""
+enotni_csv()
